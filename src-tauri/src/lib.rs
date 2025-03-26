f298920f1f14059f9795b326145db1c9c7fa6a64
CHAMP010 <yashpc09@gmail.com>
2025-03-26 17:56:15 +0530

    autostart

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -55,6 +55,29 @@ pub fn run() {
             _ => {}
         })
         .setup(|app| {
+            #[cfg(desktop)]
+            {
+                use tauri_plugin_autostart::MacosLauncher;
+                use tauri_plugin_autostart::ManagerExt;
+
+                app.handle().plugin(tauri_plugin_autostart::init(
+                    MacosLauncher::LaunchAgent,
+                    Some(vec!["--flag1", "--flag2"]),
+                ));
+
+                // Get the autostart manager
+                let autostart_manager = app.autolaunch();
+                // Enable autostart
+                let _ = autostart_manager.enable();
+                // Check enable state
+                println!(
+                    "registered for autostart? {}",
+                    autostart_manager.is_enabled().unwrap()
+                );
+                // Disable autostart
+                let _ = autostart_manager.disable();
+            }
+
             let quit_i = MenuItem::with_id(app, "quit", "Quit", true, None::<&str>)?;
             let open_i = MenuItem::with_id(app, "open", "Open", true, None::<&str>)?;
             let menu = Menu::with_items(app, &[&open_i, &quit_i])?;
